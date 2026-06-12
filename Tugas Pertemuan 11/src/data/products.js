5b8bb3650a5729890a8739cc0fbdf9b41b8074f4
PwRafli <rafliefendymm@gmail.com>
2026-06-12 19:55:42 +0700

    Add files via upload

---
+++ Tugas Pertemuan 11/src/data/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "Kaos Hitam Premium",
+    price: 125000,
+    description: "Kaos polos premium berbahan 100% cotton combed 30s yang super adem, lembut, dan nyaman dipakai seharian.",
+    image: "/images/kaoshitam.png",
+    category: "Pakaian"
+  },
+  {
+    id: 2,
+    name: "Sepatu Keren Sporty",
+    price: 499000,
+    description: "Sneakers modern dengan desain sporty dan ergonomis. Dilengkapi bantalan empuk untuk kenyamanan aktivitas harian Anda.",
+    image: "/images/sepatukeren.png",
+    category: "Sepatu"
+  },
+  {
+    id: 3,
+    name: "Topi Trendy Streetwear",
+    price: 95000,
+    description: "Baseball cap dengan strap adjustable. Desain simpel namun trendy untuk melengkapi gaya casual streetwear Anda.",
+    image: "/images/topitrendy.png",
+    category: "Aksesoris"
+  }
+];
+
+export default products;
